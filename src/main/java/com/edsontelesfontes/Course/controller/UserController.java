ac4bc5775a9ecca083b4d87a80748b1ccc64c0ed
José Edson <edson.teles.fontes@gmail.com>
2022-12-01 16:48:28 -0300

    Creating User save method and make created uri

--- src/main/java/com/edsontelesfontes/Course/controller/UserController.java
+++ src/main/java/com/edsontelesfontes/Course/controller/UserController.java
@@ -4,11 +4,10 @@ import com.edsontelesfontes.Course.entities.User;
 import com.edsontelesfontes.Course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +24,12 @@ public class UserController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user){
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
--- src/main/java/com/edsontelesfontes/Course/services/UserService.java
+++ src/main/java/com/edsontelesfontes/Course/services/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not found"));
     }
+
+    public User save(User user){
+        return userRepository.save(user);
+    }
 }
